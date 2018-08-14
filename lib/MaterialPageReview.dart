552386dbf5ab03858c07f43fc4260f6ec4f0ef94
James Nguyen (JTN) <James.Nguyen@capgroup.com>
2018-08-13 21:49:30 -0700

    fill in rest of sample code.

--- lib/MaterialPageReview.dart
+++ lib/MaterialPageReview.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:hello_world/page_dragger.dart';
+import 'package:hello_world/page_reveal.dart';
+import 'package:hello_world/pager_indicator.dart';
 import 'package:hello_world/pages.dart';
 
 class MaterialPageReview extends StatefulWidget {
@@ -8,7 +12,72 @@ class MaterialPageReview extends StatefulWidget {
       new _MaterialPageReviewState();
 }
 
-class _MaterialPageReviewState extends State<MaterialPageReview> {
+class _MaterialPageReviewState extends State<MaterialPageReview> with TickerProviderStateMixin {
+
+  StreamController<SlideUpdate> slideUpdateStream;
+  AnimatedPageDragger animatedPageDragger;
+
+  int activeIndex = 0;
+  int nextPageIndex = 0;
+  SlideDirection slideDirection = SlideDirection.none;
+  double slidePercent = 0.0;
+
+  _MaterialPageReviewState() {
+    slideUpdateStream = new StreamController<SlideUpdate>();
+
+    slideUpdateStream.stream.listen((SlideUpdate event) {
+      setState(() {
+        if (event.updateType == UpdateType.dragging) {
+          print('Sliding ${event.direction} at ${event.slidePercent}');
+          slideDirection = event.direction;
+          slidePercent = event.slidePercent;
+
+          if (slideDirection == SlideDirection.leftToRight) {
+            nextPageIndex = activeIndex - 1;
+          } else if (slideDirection == SlideDirection.rightToLeft) {
+            nextPageIndex = activeIndex + 1;
+          } else {
+            nextPageIndex = activeIndex;
+          }
+        } else if (event.updateType == UpdateType.doneDragging) {
+          print('Done dragging.');
+          if (slidePercent > 0.5) {
+            animatedPageDragger = new AnimatedPageDragger(
+              slideDirection: slideDirection,
+              transitionGoal: TransitionGoal.open,
+              slidePercent: slidePercent,
+              slideUpdateStream: slideUpdateStream,
+              vsync: this,
+            );
+          } else {
+            animatedPageDragger = new AnimatedPageDragger(
+              slideDirection: slideDirection,
+              transitionGoal: TransitionGoal.close,
+              slidePercent: slidePercent,
+              slideUpdateStream: slideUpdateStream,
+              vsync: this,
+            );
+
+            nextPageIndex = activeIndex;
+          }
+
+          animatedPageDragger.run();
+        } else if (event.updateType == UpdateType.animating) {
+          print('Sliding ${event.direction} at ${event.slidePercent}');
+          slideDirection = event.direction;
+          slidePercent = event.slidePercent;
+        } else if (event.updateType == UpdateType.doneAnimating) {
+          print('Done animating. Next page index: $nextPageIndex');
+          activeIndex = nextPageIndex;
+
+          slideDirection = SlideDirection.none;
+          slidePercent = 0.0;
+
+          animatedPageDragger.dispose();
+        }
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +85,28 @@ class _MaterialPageReviewState extends State<MaterialPageReview> {
       body: new Stack(
         children: [
           new Page(
-
+            viewModel: pages[activeIndex],
+            percentVisible: 1.0,
+          ),
+          new PageReveal(
+            revealPercent: slidePercent,
+            child: new Page(
+              viewModel: pages[nextPageIndex],
+              percentVisible: slidePercent,
+            ),
+          ),
+          new PagerIndicator(
+            viewModel: new PagerIndicatorViewModel(
+              pages,
+              activeIndex,
+              slideDirection,
+              slidePercent,
+            ),
+          ),
+          new PageDragger(
+            canDragLeftToRight: activeIndex > 0,
+            canDragRightToLeft: activeIndex < pages.length - 1,
+            slideUpdateStream: this.slideUpdateStream,
           ),
         ],
       ),
--- lib/pages.dart
+++ lib/pages.dart
@@ -2,43 +2,101 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
+final pages = [
+  new PageViewModel(Color(0xFF678FB4),
+      'assets/hotels.png',
+      'Hotels',
+      'All hotels and hostels are sorted by hospitality rating',
+      'assets/key.png'
+  ),
+  new PageViewModel(Color(0xFF65B0B4),
+      'assets/banks.png',
+      'Banks',
+      'We carefully verify all banks before adding them into the app',
+      'assets/wallet.png'
+  ),
+  new PageViewModel(Color(0xFF9B90BC),
+      'assets/stores.png',
+      'Stores',
+      'All local stores are categorized for your convenience',
+      'assets/shopping_cart.png'
+  ),
+];
 class Page extends StatelessWidget {
+
+  final PageViewModel viewModel;
+  final double percentVisible;
+
+
+  Page({
+    this.viewModel,
+    this.percentVisible = 1.0,
+  });
  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
    return new Container(
      width: double.infinity,
-     color: Colors.blue,
+     color: viewModel.color,
      child: new Column(
        mainAxisAlignment: MainAxisAlignment.center,
        children: <Widget>[
-         Padding(
-           padding: new EdgeInsets.only(bottom: 25.0),
-           child: new Image.asset('assets/hotels.png', width: 200.0, height: 200.0
+         new Transform(
+           transform: new Matrix4.translationValues(0.0, 50.0 * (1.0 - percentVisible), 0.0),
+           child: new Padding(
+              padding: new EdgeInsets.only(bottom: 25.0),
+              child: new Image.asset(
+                  viewModel.heroAssetPath,
+                  width: 200.0,
+                  height: 200.0
+               ),
            ),
          ),
-         Padding(
-           padding: const EdgeInsets.only(top: 10.0),
-           child: new Text('Hotels',
-             style: new TextStyle(
-               color: Colors.white,
-               fontFamily: 'FlamanteRoma',
-               fontSize: 34.0,
+         new Transform(
+           transform: new Matrix4.translationValues(0.0, 30.0 * (1.0 - percentVisible), 0.0),
+           child: new Padding(
+             padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+             child: new Text(
+               viewModel.title,
+               style: new TextStyle(
+                 color: Colors.white,
+                 fontFamily: 'FlamanteRoma',
+                 fontSize: 34.0,
+               ),
              ),
            ),
          ),
-         Padding(
-           padding: const EdgeInsets.only(bottom: 75.0),
-           child: new Text('This is the body',
+
+         new Transform(
+           transform: new Matrix4.translationValues(0.0, 30.0 * (1.0 - percentVisible), 0.0),
+           child: new Padding(
+              padding: const EdgeInsets.only(bottom: 75.0),
+           child: new Text(
+             viewModel.body,
              textAlign: TextAlign.center,
              style: new TextStyle(
                color: Colors.white,
                fontSize: 18.0,
              ),),
-         )
+            ),
+         ),
        ],
      ),
    );
   }
 }
 
+class PageViewModel {
+  final Color color;
+  final String heroAssetPath;
+  final String title;
+  final String body;
+  final String iconAssetIcon;
+
+  PageViewModel(
+      this.color,
+      this.heroAssetPath,
+      this.title,
+      this.body,
+      this.iconAssetIcon,
+      );
+}
